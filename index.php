642e935ab8de593ec491765c9ce6660515d26464
Yasz Avellia <zakamaragames@gmail.com>
2022-09-05 09:21:19 +0700

    menambah navbar

---
+++ index.php
@@ -0,0 +1,15 @@
+<?php 
+//helper
+include "app/function.php";
+
+//views
+include "app/views.php";
+
+//header
+include "layouts/auth/header.php";
+
+// footer
+include "layouts/auth/footer.php";
+
+
+?>
