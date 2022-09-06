7d6e79db416e0ebb56225db25324320a8a2da740
Yasz Avellia <zakamaragames@gmail.com>
2022-09-06 09:18:47 +0700

    fix text mid

--- views/beranda/index.php
+++ views/beranda/index.php
@@ -20,6 +20,10 @@
         <div class="carousel-inner">
             <div class="carousel-item active" data-bs-interval="10000">
                 <img src="assets/img/bc.jpg" class="d-block w-100" alt="...">
+                <div class="carousel-caption  d-md-block">
+                    <h1>First slide label</h1>
+                    <p>Some representative placeholder content for the first slide.</p>
+                </div>
             </div>
             <div class="carousel-item" data-bs-interval="2000">
                 <img src="assets/img/kny.png" class="d-block w-100" alt="...">
